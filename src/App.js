4dd1ca95aabafd3b6e03d905da08214de9b988b4
shafali03 <shaf03sm@gmail.com>
2020-04-27 20:00:30 +0100

    feat: View day, month and year

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
   return (
     <div className="app">
       <main>
@@ -9,6 +25,10 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">London, UK</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
