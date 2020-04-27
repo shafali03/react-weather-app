d84b81540890b978a65c271ec8ff50fc215104e7
shafali03 <shaf03sm@gmail.com>
2020-04-27 20:06:56 +0100

    html: Added mark up for weather box

--- src/App.js
+++ src/App.js
@@ -30,6 +30,12 @@ function App() {
           <div className="location">London, UK</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
+        <div className="weather-box">
+          <div className="temp">
+            15'c
+          </div>
+          <div className="weather">Sunny</div>
+        </div>
       </main>
     </div>
   );
